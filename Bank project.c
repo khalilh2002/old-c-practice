65ad4ab0af244a0bd812afd21b06792d546d9f60
Khalil El Houssine <71839189+khalilh2002@users.noreply.github.com>
2023-02-18 17:22:28 +0100

    Add files via upload

--- Bank project.c
+++ Bank project.c
@@ -94,6 +94,18 @@ void afficher(list *l){
     free(courant);
 
 }
+void afficher_file(FILE *f){
+    rewind(f);
+    char n[20],p[10];
+    double d;
+    while (feof(f)==NULL)
+    {
+        fscanf(f,"%s %s %lf",n,p,&d);
+        printf("%s %s %lf\n",n,p,d);
+    }
+
+}
+
 void copy_list(list *l , FILE *f){
     rewind(f);
     mailon *courant ;
@@ -102,11 +114,12 @@ void copy_list(list *l , FILE *f){
     {
         printf("list vide error");
     }
-    while (feof(f)==NULL && (courant != NULL))
+    while ((courant != NULL))
     {
         fprintf(f,"%s %s %lf\n",courant->nom,courant->pass,courant->val);
         courant=courant->suivant;
     }
+    free(courant);
 }
 
 int uservrai(FILE *f,char user[20]){
@@ -125,6 +138,22 @@ int uservrai(FILE *f,char user[20]){
     }//1 exit 0 n exit pas
     return t;
 }
+int pasverai(FILE *f,char pass[10]){
+    int n,t;
+    double pr;
+    t=0;
+    char user2[20],bin[10];
+    rewind(f);
+    while(feof(f)==NULL){
+        fscanf(f,"%s %s %lf",&user2,&bin,&pr);
+        n=strcmp(bin,pass);
+        if(n==0){
+           t=1;
+           break;
+        }
+    }//1 exit 0 n exit pas
+    return t ;
+}
 double montant(FILE *f ,char name[20]){
     double n;
     char us[20],ps[10];
@@ -136,13 +165,43 @@ double montant(FILE *f ,char name[20]){
         {
             break;
         }
-        
+
     }
-    
+
     return n;
+}
+void verser(FILE *f,char name[20],double n){
+    list *l;
+    l=cree_list();
+    rewind(f);
+    if (l==NULL)
+    {
+        printf("eror sur list verser");
+        exit(0);
+    }
+    char us[20],ps[10];
+    double i;
+    while (feof(f)==NULL)
+    {
+        fscanf(f,"%s %s %lf",us,ps,&i);
+        if (strcmp(us,name)==0)
+        {
+            l=remplire_list(l,i+n,us,ps);
+        }else
+        {
+            l=remplire_list(l,i,us,ps);
+        }
+
+    }
+    //afficher(l);
+    copy_list(l,f);
+    //afficher_file(f);
+    free(l);
+    printf("\n***verser complete");
 }
 
 int main(){
+    system("color b0");
     FILE *f ;
     char username[20],password[10];
     int x,v;
@@ -155,19 +214,10 @@ int main(){
     }
     list *l;
     l=cree_list();
-    /*new code strat here */
-        
-        /*while (feof(f)==NULL)
-        {
-            fscanf(f,"%s %s %lf",username,password,&mony);
-            l=remplire_list(l,mony,username,password);
-        }
-        afficher(l);
-        getchar();
-        exit(0);
-    /*NEW CODE END HERE*/
+
     printf("creat account (1) ou entrer accont (2) : ");
     scanf("%d",&x);
+    system("cls");
     if(x==1){
        do{
             printf("username : ");
@@ -175,97 +225,186 @@ int main(){
             printf("password : ");
             scanf("%s",&password);
             v=uservrai(f,username);
+            if (v==0)
+            {
+                break;
+            }
+            printf("error le username exit \n");
+
         }while(v==1);
         f=fopen("user_info.txt","a+");
         fprintf(f,"%s %s %lf\n",username,password,mony);
-    }else{
-        if(x==2){
-       do{
-            printf("username : ");
-            scanf("%s",&username);
-            printf("password : ");
-            scanf("%s",&password);
-            v=uservrai(f,username);
-            if(v==0){
-                printf("\n username n'exist pas,\n essayer a nouveau\n\n");
-            }
-        }while(v==0);
-
-            printf("voir le montant (1) ou transformer (2): ");
-            scanf("%d",&v);
-            char user2[20],pas2[10];
-            if (v==1)
+        /*systeme cls*/
+        printf("\ncomplete....pour continue press ENTER");
+        getch();
+        system("cls");
+        printf("est ce que tu veut verser a ton accont oui(1) ou non(2)? : ");
+        int i;
+        fclose(f);
+        scanf("%d",&i);
+        system("cls");
+        if (i==1)
+        {
+            double money;
+            f=fopen("user_info.txt","r+");
+            do
             {
-               /* rewind(f);
-                double money;
-                while (feof(f)==NULL)
+                printf("donner le montant : ");
+                scanf("%lf",&money);
+                if (money > 0)
                 {
-                    fscanf(f,"%s %s %lf",user2,pas2,&money);
-                    if (strcmp(user2,username)==0)
-                    {
-                        break;
-                    }
-                }*/
-                printf("le montant est : %lf DH",montant(f,username));
-
-            }else if (v==2)
-            {
-                double ver,n; //recode versemnt efacer et remplire !!!!!!
-                char usr[20];
-                int t=0;                
-                do/*verfier si username exit*/
-                {
-                    printf("donne le userame d arrive ");
-                    scanf("%s",usr);
-                    t=uservrai(f,usr); 
-                    if (t==1)
-                    {
-                        break;
-                    }
-                    printf("username n'exit pas recrie :********\n ");
-
-                } while (t!=1);
-                
-                do/*verier le montant exixt dans l'accont*/
-                {
-                    printf("donnre le montant pour trnsformer : ");
-                    scanf("%lf",&ver);
-                    if (montant(f,username) >= ver && ver > 0 )
-                    {
-                        break;
-                    }else if (ver > montant(f,username))
-                    {
-                        printf("le montant de trasnforeme est plus grand \n ton montant:%lf dh\n",montant(f,username));
-                    }else
-                    {
-                        printf("error le nombre est negative , recrire le montat \n");
-                    }
-                    
-                } while ( (ver > montant(f,username)) || (0 > ver) );
-                
-                rewind(f);
-                while (feof(f)==NULL)
-                {
-                   fscanf(f,"%s %s %lf",user2,pas2,&n);
-                   if (strcmp(user2,usr)==0)
-                   {
-                        l=remplire_list(l,n+ver,user2,pas2);
-                   }
-                   else if (strcmp(user2,username)==0)
-                   {
-                        l=remplire_list(l,n-ver,user2,pas2);
-                   }
-                   else
-                   {
-                        l=remplire_list(l,n,user2,pas2);
-                   }
-
+                    break;
                 }
-                //afficher(l);
-                copy_list(l,f);
-                printf("transformer est complete : \n le montant qui rest  est : %lf DH",montant(f,username));
+                printf("error le montant est negative !!!\n");
+            } while ( 0 >= money);
+            verser(f,username,money);
+        }
 
-            }
+    }else if(x==2){
+        if(x==2){
+            int p=0;
+            do{
+                printf("username : ");
+                scanf("%s",&username);
+                printf("password : ");
+                scanf("%s",&password);
+                v=uservrai(f,username);
+                p=pasverai(f,password);
+                if(v==0){
+                    system("cls");
+                    printf("\n username n'exist pas,\n essayer a nouveau\n\n");
+                }
+                if(p==0 && v==1 ){
+                    system("cls");
+                    printf("\n password est faux,\n essayer a nouveau\n\n");
+                }
+            }while(v==0 || p==0);
+            /*system cls*/
+            printf(" complete....pour continue press ENTER");
+            getch();
+            system("cls");
+                int v1=0;
+            do //retour et exit
+            {
+            printf("voir le montant (1) ou transformer (2) ou verser(3): ");
+            scanf("%d",&v);
+            system("cls");
+            char user2[20],pas2[10];
+
+                if (v==1)
+                {
+                    printf("le montant est : %lf DH",montant(f,username));
+                    printf("\n retour (0) ou exit(1) : ");/*retour et exit*/
+                    scanf("%d",&v1);
+                    system("cls");
+                }else if (v==2)
+                {
+                    double ver,n;
+                    char usr[20];
+                    int t=0;
+                    do/*verfier si username exit*/
+                    {
+                        printf("donne le userame d arrive ");
+                        scanf("%s",usr);
+                        t=uservrai(f,usr);
+                        if (t==1)
+                        {
+                            break;
+                        }
+                        printf("username n'exit pas recrie :********\n ");
+
+                    } while (t!=1);
+
+                    do/*verier le montant exixt dans l'accont*/
+                    {
+                        printf("donnre le montant pour trnsformer : ");
+                        scanf("%lf",&ver);
+                        if (montant(f,username) >= ver && ver > 0 )
+                        {
+                            break;
+                        }else if (ver > montant(f,username))
+                        {
+                            printf("le montant de trasnforeme est plus grand \n ton montant:%lf dh\n",montant(f,username));
+                        }else
+                        {
+                            printf("error le nombre est negative ou '0' , recrire le montat \n");
+                        }
+
+                    } while ( (ver > montant(f,username)) || (0 >= ver) );
+
+                    rewind(f);
+                    while (feof(f)==NULL)
+                    {
+                    fscanf(f,"%s %s %lf",user2,pas2,&n);
+                    if (strcmp(user2,usr)==0)
+                    {
+                            l=remplire_list(l,n+ver,user2,pas2);
+                    }
+                    else if (strcmp(user2,username)==0)
+                    {
+                            l=remplire_list(l,n-ver,user2,pas2);
+                    }
+                    else
+                    {
+                            l=remplire_list(l,n,user2,pas2);
+                    }
+
+                    }
+                    //afficher(l);
+                    copy_list(l,f);
+                    printf("transformer est complete : \n le montant qui rest  est : %lf DH",montant(f,username));
+                    printf("\n retour (0) ou exit(1) : ");/*retour et exit*/
+                    scanf("%d",&v1);
+                    system("cls");
+
+                }else if (v==3)
+                {
+                    fclose(f);
+                    f=fopen("user_info.txt","r+");
+                    if (f==NULL)
+                    {
+                        printf("error sur le ficher v3 ");
+                        exit(0);
+                    }
+                    double ver;
+                    char name[20];
+                    v=0;
+                    do
+                    {
+                        printf("username pour verser : ");
+                        scanf("%s",name);
+                        v=uservrai(f,name);
+                        if (v==1)
+                        {
+                            break;
+                        }
+                        printf("error username n'exit pas\n");
+
+                    } while (v==0);
+                    v=0;
+                    do
+                    {
+                        printf("le montant : ");
+                        scanf("%lf",&ver);
+                        if ( 0 > ver)
+                        {
+                            printf("\n*montant negative recrire \n");
+                        }
+
+                    } while (0 > ver);
+
+                    verser(f,name,ver);
+                    system("cls");
+                    printf("complete...");
+                    printf("\n retour (0) ou exit(1) : ");/*retour et exit*/
+                    scanf("%d",&v1);
+                    system("cls");
+
+                }else
+                {
+                    printf("error le choix n'exit pas \n");
+                }
+            } while ( v1==0);
 
         }
 
